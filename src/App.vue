2f5b6142c91620718fdbc92c83c2b70d13193dfb
nhaga <nhaga5@gmail.com>
2022-12-28 11:12:06 +0000

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="p-10">
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <HelloWorld msg="Vite + Vue" />
+  <router-view></router-view>
+
+  </div>
+</template>
+
+<style>
+</style>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import './style.css'
+import App from './App.vue'
+import router from './router.js'
+
+const pinia = createPinia()
+
+
+createApp(App).use(pinia).use(router).mount('#app')
